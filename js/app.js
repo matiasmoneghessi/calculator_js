edc01c2a1257cc31c6d1c04757c5d2529fa2aa3f
matiasmoneghessi <matimone9@gmail.com>
2020-06-17 19:57:52 -0300

    insert buttons

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const screen = document.getElementById('calculatorScreen'),
+    keys = document.getElementById('calculatorKeys')
+
+const calculator = () => {
+    if (!keys) return
+    keys.addEventListener('click', () => {
+
+    })
+}
