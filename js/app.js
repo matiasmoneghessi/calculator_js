1542825c02dd9b212e87bd0af72dad805004c61a
matiasmoneghessi <matimone9@gmail.com>
2020-06-17 20:31:00 -0300

    add reset.css and fix style.css

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const screen = document.getElementById('calculatorScreen'),
-    keys = document.getElementById('calculatorKeys')
-
-const calculator = () => {
-    if (!keys) return
-    keys.addEventListener('click', () => {
-
-    })
-}
